44d99400a30fbaedbbf5ebb9a58dc3f67ef38ea2
Albert Garuda <algarud@gmail.com>
2022-09-08 00:31:23 +0300

    contract minchanges

--- packages/hardhat-ts/contracts/DixiNFT.sol
+++ packages/hardhat-ts/contracts/DixiNFT.sol
@@ -15,6 +15,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract DixiNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
+  uint256 _currentPrice = 0.01;
 
   mapping(uint256 => bytes32) public _inputHashes; // private
   mapping(uint256 => bool) public _gameParticipation;
@@ -23,6 +24,12 @@ contract DixiNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
   function transferOwnership(address owner) onlyOwner {
     transferOwnership(owner);
+  }
+
+  function withdraw(uint256 amount) public payable {
+    require(balances[msg.sender] >= amount, "not enough funds");
+    balances[msg.sender] -= amount;
+    payable(msg.sender).transfer(amount);
   }
 
   function _baseURI() internal view virtual override returns (string memory) {
@@ -50,13 +57,19 @@ contract DixiNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
   }
 
   function mintItem(address to, bytes32 inputHash) public returns (uint256) {
+    require(msg.value >= _currentPrice, "Please check current price, it's already higher!");
+
     _tokenIds.increment();
     uint256 id = _tokenIds.current();
     _inputHashes[id] = inputHash;
+    _owners[id] = to;
     return id;
   }
 
   function mintFinalize(uint256 id, string memory tokenURI) public onlyOwner returns (uint256) {
+    // @! change onlyOwner to special sig
+
+    address to = _owners[id];
     _mint(to, id);
     _setTokenURI(id, tokenURI);
     return id;
@@ -72,6 +85,8 @@ contract DixiNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     _gameParticipation[id] = false;
   }
 
+  // @! stake fight
+
   function takeOver(
     bytes sig,
     uint256 id,
@@ -79,5 +94,7 @@ contract DixiNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
   ) {
     require(_gameParticipation[id], "This NFT owner decided not to participate in the Game");
     // @! check sig and transfer ownership
+
+    // @! give new generation ticket
   }
 }
