afe1c09fa5c8799ac72ad1a72980841c1abb4783
Albert Garuda <algarud@gmail.com>
2022-09-08 00:42:25 +0300

    contract improvements

--- packages/hardhat-ts/contracts/DixiNFT.sol
+++ packages/hardhat-ts/contracts/DixiNFT.sol
@@ -15,21 +15,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract DixiNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
-  uint256 _currentPrice = 0.01;
+  uint256 _currentPrice = 0.01 ether;
 
   mapping(uint256 => bytes32) public _inputHashes; // private
+  mapping(uint256 => address) public _minters;
   mapping(uint256 => bool) public _gameParticipation;
 
   constructor() ERC721("DixiNFT", "DIXI") {}
 
-  function transferOwnership(address owner) onlyOwner {
+  function transferOwnership(address owner) public override(Ownable) onlyOwner {
     transferOwnership(owner);
   }
 
-  function withdraw(uint256 amount) public payable {
-    require(balances[msg.sender] >= amount, "not enough funds");
-    balances[msg.sender] -= amount;
-    payable(msg.sender).transfer(amount);
+  function withdraw(uint256 amount) public payable onlyOwner {
+    payable(owner()).transfer(amount);
   }
 
   function _baseURI() internal view virtual override returns (string memory) {
@@ -62,36 +61,37 @@ contract DixiNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     _tokenIds.increment();
     uint256 id = _tokenIds.current();
     _inputHashes[id] = inputHash;
-    _owners[id] = to;
+    _minters[id] = to;
     return id;
   }
 
   function mintFinalize(uint256 id, string memory tokenURI) public onlyOwner returns (uint256) {
     // @! change onlyOwner to special sig
 
-    address to = _owners[id];
+    _currentPrice = _currentPrice + _currentPrice / 100;
+    address to = _minters[id];
     _mint(to, id);
     _setTokenURI(id, tokenURI);
     return id;
   }
 
-  function enterTheGame(uint256 id) {
-    require(_owners[id] == msg.sender, "Only NFT owner can enter the Game");
+  function enterTheGame(uint256 id) public {
+    require(_minters[id] == msg.sender, "Only NFT owner can enter the Game");
     _gameParticipation[id] = true;
   }
 
-  function exitGame(uint256 id) {
-    require(_owners[id] == msg.sender, "Only owner can take his NFT off the Game");
+  function exitGame(uint256 id) public {
+    require(_minters[id] == msg.sender, "Only owner can take his NFT off the Game");
     _gameParticipation[id] = false;
   }
 
   // @! stake fight
 
   function takeOver(
-    bytes sig,
+    bytes memory sig,
     uint256 id,
     bytes32 hash
-  ) {
+  ) public {
     require(_gameParticipation[id], "This NFT owner decided not to participate in the Game");
     // @! check sig and transfer ownership
 
