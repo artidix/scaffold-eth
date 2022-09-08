8f4db500c56de91350e152d60f793dc143a0c84f
Albert Garuda <algarud@gmail.com>
2022-09-08 21:50:17 +0300

    contract verify lib

--- packages/hardhat-ts/contracts/DixiNFT.sol
+++ packages/hardhat-ts/contracts/DixiNFT.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./lib/Verify.sol";
 
 //learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
 
